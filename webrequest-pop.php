6b9201c5d9fce8db23959ec561753d5a369deeb2
Francesco Bianco <info.francescobianco@gmail.com>
2022-02-22 09:15:12 +0100

    First commit

--- webrequest-pop.php
+++ webrequest-pop.php
@@ -1,8 +1,18 @@
 <?php
 
-var_dump($_POST);
+$sheet1 = $_POST['sheet1'];
+$authorization =
 
-echo "Ciao";
+// Pop row from sheet1
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, $sheet1);
+curl_setopt($curl, CURLOPT_HTTPHEADER, ['Content-Type: application/json' , $authorization ]);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+$row1 = curl_exec($curl);
+curl_close($curl);
+
+var_dump($row1);
+
 
 
 
